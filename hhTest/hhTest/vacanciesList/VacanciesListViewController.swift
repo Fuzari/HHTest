32a0d7119b48757947cf37a65361c0fcddbe8791
Andrey Yakovlev <andrey.e.yakovlev@megafon.ru>
2021-09-18 17:35:23 +0300

    Add initial project

---
+++ hhTest/hhTest/vacanciesList/VacanciesListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  VacanciesListViewController.swift
+//  hhTest
+//
+//  Created by Андрей Яковлев on 18.09.2021.
+//
+
+import UIKit
+
+final class VacanciesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
