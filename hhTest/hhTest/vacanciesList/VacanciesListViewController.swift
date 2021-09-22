3accddbff62649c021e648a81a614f65de005348
Andrey Yakovlev <andrey.e.yakovlev@megafon.ru>
2021-09-23 00:35:43 +0300

    Add tableView

--- hhTest/hhTest/vacanciesList/VacanciesListViewController.swift
+++ hhTest/hhTest/vacanciesList/VacanciesListViewController.swift
@@ -8,9 +8,35 @@
 import UIKit
 
 final class VacanciesListViewController: UIViewController {
-
+    private let vacanciesTableView: UITableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViews()
+        layoutSubViews()
+    }
+    
+    private func configureViews() {
+        configureVacanciesTableView()
+        applyTheme()
+    }
+    
+    private func configureVacanciesTableView() {
+        vacanciesTableView.separatorStyle = .none
+    }
+    
+    private func layoutSubViews() {
+        layoutVacanciesTableView()
+    }
+    
+    private func layoutVacanciesTableView() {
+        view.addSubview(vacanciesTableView)
+        vacanciesTableView.fillToSuperview()
+    }
+    
+    private func applyTheme() {
+        view.backgroundColor = .clear
+        vacanciesTableView.backgroundColor = .mainBg
     }
 }
 
