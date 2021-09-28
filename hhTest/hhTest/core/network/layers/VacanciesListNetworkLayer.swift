09ddfc27731a72b077189d2fecc9c43171478fc9
Andrey Yakovlev <andrey.e.yakovlev@megafon.ru>
2021-09-28 23:20:38 +0300

    Introduce  VacanciesListNetworkLayer

---
+++ hhTest/hhTest/core/network/layers/VacanciesListNetworkLayer.swift
@@ -0,0 +1,48 @@
+//
+//  VacanciesListNetworkLayer.swift
+//  hhTest
+//
+//  Created by Андрей Яковлев on 28.09.2021.
+//
+
+import Foundation
+
+typealias VacanciesResponseResult = Result<VacanciesResponseModel, NetworkError>
+typealias VacanciesResponseCompletion = ParameterClosure<VacanciesResponseResult>
+
+protocol IVacanciesListNetworkLayer {
+    func fetchVacanciesList(completion: @escaping VacanciesResponseCompletion)
+}
+
+final class VacanciesListNetworkLayer: IVacanciesListNetworkLayer {
+    
+    private let executor: IRequestExecutor
+    
+    init(executor: IRequestExecutor) {
+        self.executor = executor
+    }
+    
+    func fetchVacanciesList(completion: @escaping VacanciesResponseCompletion) {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = HHApiScheme.https.rawValue
+        urlComponents.host = HHApiHost.main.rawValue
+        urlComponents.path = "/\(HHApiResources.vacancies.rawValue)"
+        let parameters: [String : String] = ["text": "iOS", "per_page": "2"]
+        urlComponents.setQueryItems(parameters: parameters)
+        
+        guard let url = urlComponents.url else {
+            completion(.failure(.clientError))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        executor.execute(request: request, with: VacanciesResponseModel.self) { result in
+            switch result {
+            case .success(let responseModel):
+                completion(.success(responseModel))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
