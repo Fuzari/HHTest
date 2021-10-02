a63eb8be9b24e32e74492b48444d593ecad97a1b
Andrey Yakovlev <andrey.e.yakovlev@megafon.ru>
2021-10-03 00:10:15 +0300

    Remove unused code

--- hhTest/hhTest/vacanciesList/VacanciesListViewController.swift
+++ hhTest/hhTest/vacanciesList/VacanciesListViewController.swift
@@ -7,46 +7,14 @@
 
 import UIKit
 
-final class VacanciesListViewController: UIViewController, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let model = responseModel else {
-            return 0
-        }
-        
-        return model.items.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCellWith(type: VacancyCell.self, for: indexPath) else {
-            fatalError("Received unexpected nil value \(#file) \(#line)")
-        }
-        
-        cell.setup(vacancy: responseModel!.items[indexPath.row])
-        return cell
-    }
-    
+final class VacanciesListViewController: UIViewController {
     private let vacanciesSearchBar: UISearchBar = UISearchBar()
     private let vacanciesTableView: UITableView = UITableView()
-    
-    private let layer = VacanciesListNetworkLayer()
-    private var responseModel: IVacanciesResponseModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViews()
         layoutViews()
-        
-        layer.fetchVacanciesList { result in
-            switch result {
-            case .success(let model):
-                self.responseModel = model
-                DispatchQueue.main.async {
-                    self.vacanciesTableView.reloadData()
-                }
-            case .failure( _ ):
-                print("Error")
-            }
-        }
     }
     
     private func configureViews() {
@@ -65,7 +33,6 @@ final class VacanciesListViewController: UIViewController, UITableViewDataSource
     private func configureVacanciesTableView() {
         vacanciesTableView.separatorStyle = .none
         vacanciesTableView.registerCellWith(type: VacancyCell.self)
-        vacanciesTableView.dataSource = self
     }
     
     private func layoutViews() {
