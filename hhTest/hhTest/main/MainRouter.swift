7a98d90f0de7e551337efb3473520e8356069b19
Andrey Yakovlev <andrey.e.yakovlev@megafon.ru>
2021-10-04 14:20:33 +0300

    Introduce initial screen

---
+++ hhTest/hhTest/main/MainRouter.swift
@@ -0,0 +1,32 @@
+//
+//  MainRouter.swift
+//  hhTest
+//
+//  Created by Андрей Яковлев on 04.10.2021.
+//
+
+import UIKit
+
+protocol IMainRouter {
+    var context: UIViewController? { get }
+    func showVacanciesSearch()
+}
+
+final class MainRouter: IMainRouter {
+    private(set) weak var context: UIViewController?
+    
+    init(context: UIViewController) {
+        self.context = context
+    }
+    
+    func showVacanciesSearch() {
+        let executor = RequestExecutor(session: URLSession(configuration: .default))
+        let layer = VacanciesListNetworkLayer(executor: executor)
+        let vm = VacanciesListViewModel(networkLayer: layer)
+        let controller = VacanciesListViewController(viewModel: vm)
+        let nc = UINavigationController(rootViewController: controller)
+        nc.modalTransitionStyle = .coverVertical
+        nc.modalPresentationStyle = .overFullScreen
+        context?.present(nc, animated: true, completion: nil)
+    }
+}
